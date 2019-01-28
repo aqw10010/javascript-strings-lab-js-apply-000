e2c0936f9b290558634e1e704d9582d25bff22bf
Austin Williams <austinq.williams@gmail.com>
2019-01-28 02:55:15 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 var greeting = "Hello, everybody!";
 
-var specialGuest = "Neil deGrasse Tyson" 
+var specialGuest = "Neil deGrasse Tyson" ;
 
-var greetSpecialGuest += "Neil deGrasse Tyson" + specialGuest + "Hello,!";
+var greetSpecialGuest = "Neil deGrasse Tyson" + specialGuest + "Hello,!" ;
 
 var topic = "space";
 
